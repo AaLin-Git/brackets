cacebc08a9f8dfbd6c91cb9a00327a2ae209d62e
AaLin-Git <aalin.box.it@gmail.com>
2023-01-19 23:26:24 +0100

    feat: create check for some brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  //console.log(str + ' str');
+  //console.log(bracketsConfig[0][0] + ' bracketsConfig');
+  //console.log(`${brackets.length} is length`);
+  //let result = '';
+  // for (let i = 0; i < bracketsConfig.lenght; i++) {
+  //   console.log(bracketsConfig[i][0]);
+  // }
+
+  for (const item of bracketsConfig) {
+    //console.log(item);
+    //console.log(item[0] + item[1]);
+    let brackets = str.indexOf(item[0] + item[1]);
+    //console.log(brackets);
+    return brackets === -1 ? false : true;
+  }
+  //console.log(result + ' result');
+};
