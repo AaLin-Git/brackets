4628baee8ccc0b4d875547caee1799c7f6571bf1
AaLin-Git <aalin.box.it@gmail.com>
2023-01-20 00:08:16 +0100

    feat: resolve third test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  //console.log(str + ' str');
-  //console.log(bracketsConfig[0][0] + ' bracketsConfig');
-  //console.log(`${brackets.length} is length`);
-  //let result = '';
-  // for (let i = 0; i < bracketsConfig.lenght; i++) {
-  //   console.log(bracketsConfig[i][0]);
-  // }
-
   for (const item of bracketsConfig) {
-    //console.log(item);
-    //console.log(item[0] + item[1]);
-    let brackets = str.indexOf(item[0] + item[1]);
-    //console.log(brackets);
-    return brackets === -1 ? false : true;
+    //console.log(item + ' item');
+    let brackets = item[0] + item[1];
+    //console.log(brackets)
+    let bracketsIndex = str.indexOf(item[0] + item[1]);
+    //console.log(bracketsIndex + ' brackets');
+    let newStr;
+    if (bracketsIndex === -1) {
+      return false;
+    } else if (bracketsIndex === 0 && str.length > 2) {
+      newStr = str.slice(2, str.length);
+      // console.log(newStr)
+      if (newStr.length < 3 && newStr != brackets) {
+        return false;
+      }
+    } else {
+      return true;
+    }
   }
-  //console.log(result + ' result');
 };
