2f165a3aaf1dc9f9acd20fb7b68e06a80eb9a53e
AaLin-Git <aalin.box.it@gmail.com>
2023-01-20 13:05:36 +0100

    refactor: clean code

--- src/index.js
+++ src/index.js
@@ -1,38 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // let arr = [];
-  // for (let item of bracketsConfig) {
-  //   let brackets = item[0] + item[1];
-  //   let bracketsIndex = str.indexOf(brackets);
-  //   while (bracketsIndex !== -1) {
-  //     arr = str.split("");
-  //     //console.log(arr);
-  //     //console.log(arr.splice(bracketsIndex, 2));
-  //     //console.log(arr);
-  //     str = arr.join("");
-  //     //console.log(str);
-  //     bracketsIndex = str.indexOf(brackets);
-  //     //console.log(bracketsIndex);
-  //   }
-  //   if (bracketsIndex === -1) {
-  //     return false;
-  //   }
-  // }
-
-  //console.log(bracketsConfig.length)
-
-  //let newStr = str;
-
   function deleteBrackets() {
     for (let i = 0; i < bracketsConfig.length; i++) {
-      //console.log(bracketsConfig.length , i);
-
       let brackets = bracketsConfig[i][0] + bracketsConfig[i][1];
-      //console.log(brackets + " brackets");
-
       while (str.indexOf(brackets) !== -1) {
         str = str.replace(brackets, "");
-        //console.log(str);
-        //continue;
       }
     }
     return str;
