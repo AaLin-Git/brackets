2c52a3ef57a6c66bbd2b0cecfa55d21d4605ee36
AaLin-Git <aalin.box.it@gmail.com>
2023-01-20 12:52:28 +0100

    feat: add loop with deleteBrackets function

--- src/index.js
+++ src/index.js
@@ -1,21 +1,50 @@
 module.exports = function check(str, bracketsConfig) {
-  for (const item of bracketsConfig) {
-    //console.log(item + ' item');
-    let brackets = item[0] + item[1];
-    //console.log(brackets)
-    let bracketsIndex = str.indexOf(item[0] + item[1]);
-    //console.log(bracketsIndex + ' brackets');
-    let newStr;
-    if (bracketsIndex === -1) {
-      return false;
-    } else if (bracketsIndex === 0 && str.length > 2) {
-      newStr = str.slice(2, str.length);
-      // console.log(newStr)
-      if (newStr.length < 3 && newStr != brackets) {
-        return false;
+  // let arr = [];
+  // for (let item of bracketsConfig) {
+  //   let brackets = item[0] + item[1];
+  //   let bracketsIndex = str.indexOf(brackets);
+  //   while (bracketsIndex !== -1) {
+  //     arr = str.split("");
+  //     //console.log(arr);
+  //     //console.log(arr.splice(bracketsIndex, 2));
+  //     //console.log(arr);
+  //     str = arr.join("");
+  //     //console.log(str);
+  //     bracketsIndex = str.indexOf(brackets);
+  //     //console.log(bracketsIndex);
+  //   }
+  //   if (bracketsIndex === -1) {
+  //     return false;
+  //   }
+  // }
+
+  //console.log(bracketsConfig.length)
+
+  //let newStr = str;
+
+  function deleteBrackets() {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      //console.log(bracketsConfig.length , i);
+
+      let brackets = bracketsConfig[i][0] + bracketsConfig[i][1];
+      //console.log(brackets + " brackets");
+
+      while (str.indexOf(brackets) !== -1) {
+        str = str.replace(brackets, "");
+        //console.log(str);
+        //continue;
       }
-    } else {
-      return true;
     }
+    return str;
+  }
+
+  for (let x = 0; x < 10; x++) {
+    deleteBrackets();
+  }
+
+  if (str === "") {
+    return true;
+  } else {
+    return false;
   }
 };
